25751547ba44c61aadb37cc86f4bb7e19874a819
valenciasfn <127365267+valenciasfn@users.noreply.github.com>
2023-03-09 10:11:03 +0700

    Add - Latihan 1

---
+++ 4Modul1/src/latihan1/Makanan.java
@@ -0,0 +1,8 @@
+package latihan1;
+
+//pemanfaatan inheritance
+public class Makanan extends Hidangan{
+    public String disantap(){
+        return this.getNamaHidangan() + " dimakan";
+    }
+}
---
+++ 4Modul1/src/latihan1/Minuman.java
@@ -0,0 +1,9 @@
+package latihan1;
+
+//pemanfaatan inheritance
+public class Minuman extends Hidangan{
+    public String disantap(){
+        return this.getNamaHidangan() + " diminum";
+    }
+    
+}
